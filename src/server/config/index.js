9539d5d6e727efb1b991b9b92970315735145730
Jeremy Blalock <jeremyjblalock@gmail.com>
2019-12-03 20:23:47 -0800

    Adds new queues

--- src/server/config/index.js
+++ src/server/config/index.js
@@ -1,30 +1,33 @@
 require('dotenv').config()
 
+const base = {
+  type: 'bee',
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+  password: process.env.REDIS_PASSWORD,
+}
+
 module.exports = {
-  "__example_queues": [
+  queues: [
     {
-      "name": "my_queue",
-      "port": 6381,
-      "host": "127.0.0.1",
-      "hostId": "AWS Server 2"
-    }
-  ],
-  "queues": [
-    {
-      "type": "bee",
-      "name": "adalo-compile",
-      "host": process.env.REDIS_HOST,
-      "port": process.env.REDIS_PORT,
-      "password": process.env.REDIS_PASSWORD,
-      "hostId": "Compile Queue",
+      ...base,
+      name: 'adalo-compile',
+      hostId: 'Compile Queue',
     },
     {
-      "type": "bee",
-      "name": "proton-database-updates",
-      "host": process.env.REDIS_HOST,
-      "port": process.env.REDIS_PORT,
-      "password": process.env.REDIS_PASSWORD,
-      "hostId": "Updates Queue",
+      ...base,
+      name: 'proton-database-updates',
+      hostId: 'Updates Queue',
+    },
+    {
+      ...base,
+      name: 'adaloAndroidBuilds',
+      hostId: 'Android Builds',
+    },
+    {
+      ...base,
+      name: 'adaloIOSBuilds',
+      hostId: 'iOS Builds',
     }
   ]
 }
